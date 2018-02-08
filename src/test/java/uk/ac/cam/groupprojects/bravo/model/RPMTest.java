9831548daebce3814cae675002fbcb01f20e54eb
adeboyed <david.adeboye@outlook.com>
2018-02-08 11:45:54 +0000

    Add tests for the rpm abstraction

---
+++ src/test/java/uk/ac/cam/groupprojects/bravo/model/RPMTest.java
@@ -0,0 +1,33 @@
+package uk.ac.cam.groupprojects.bravo.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by david on 08/02/2018.
+ */
+public class RPMTest {
+
+    @Test
+    public void testBounds(){
+        RPM rpm = new RPM();
+        assertEquals( false, rpm.setValue(-5) );
+        assertEquals( false, rpm.setValue(400) );
+        assertEquals( true, rpm.setValue(50) );
+    }
+
+    @Test
+    public void testOutput(){
+        RPM rpm = new RPM();
+        rpm.setValue(50);
+        assertEquals( "Your current rotations per minute is 50", rpm.speakValue() );
+
+        rpm.setValue(40);
+        assertEquals( "Your current rotations per minute is 40", rpm.speakValue() );
+
+        rpm.setValue(-40);
+        assertEquals( "Your current rotations per minute is 40", rpm.speakValue() );
+    }
+
+}
