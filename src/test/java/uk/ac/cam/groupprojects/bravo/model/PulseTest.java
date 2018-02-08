2fcc8fdf2f4675ab95a3f2769adcc6e2cfafb5a7
adeboyed <david.adeboye@outlook.com>
2018-02-08 11:33:51 +0000

    Add tests to test the Pulse abstraction

---
+++ src/test/java/uk/ac/cam/groupprojects/bravo/model/PulseTest.java
@@ -0,0 +1,33 @@
+package uk.ac.cam.groupprojects.bravo.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by david on 08/02/2018.
+ */
+public class PulseTest {
+
+    @Test
+    public void testBounds(){
+        Pulse pulse = new Pulse();
+        assertEquals( false, pulse.setValue(-5) );
+        assertEquals( false, pulse.setValue(400) );
+        assertEquals( true, pulse.setValue(50) );
+    }
+
+    @Test
+    public void testOutput(){
+        Pulse pulse = new Pulse();
+        pulse.setValue(50);
+        assertEquals( "Your current heart rate is 50 beats per minute", pulse.speakValue() );
+
+        pulse.setValue(40);
+        assertEquals( "Your current heart rate is 40 beats per minute", pulse.speakValue() );
+
+        pulse.setValue(-40);
+        assertEquals( "Your current heart rate is 40 beats per minute", pulse.speakValue() );
+    }
+
+}
