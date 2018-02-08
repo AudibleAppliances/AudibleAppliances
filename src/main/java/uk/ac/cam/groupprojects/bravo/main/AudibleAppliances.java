49951c169781b10c8499f292359faf726bb936d3
adeboyed <david.adeboye@outlook.com>
2018-02-08 12:49:04 +0000

    Started main function

---
+++ src/main/java/uk/ac/cam/groupprojects/bravo/main/AudibleAppliances.java
@@ -0,0 +1,13 @@
+package uk.ac.cam.groupprojects.bravo.main;
+
+/**
+ * Created by david on 08/02/2018.
+ */
+public class AudibleAppliances {
+
+    public static void main(String[] args) {
+
+        
+    }
+
+}
