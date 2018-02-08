ce7a35de43285f548e67e6dae8bb7ee687c204ad
adeboyed <david.adeboye@outlook.com>
2018-02-08 12:33:16 +0000

    Change everything, ngl

--- src/main/java/uk/ac/cam/groupprojects/bravo/model/Level.java
+++ src/main/java/uk/ac/cam/groupprojects/bravo/model/Level.java
@@ -11,6 +11,6 @@ public class Level extends ScreenNumber {
 
     @Override
     public String speakValue() {
-        return String.format( "Your current level is %d ", getValue() );
+        return String.format( "Your current level is %d", getValue() );
     }
 }
