c1fd05951aa84823d6d3a68bfe8fe7696fd1a92f
Keith <hnefatl@gmail.com>
2018-02-13 22:51:27 +0000

    Added serialVersionUID to serialisable classes

--- src/main/java/uk/ac/cam/groupprojects/bravo/tts/FestivalMissingException.java
+++ src/main/java/uk/ac/cam/groupprojects/bravo/tts/FestivalMissingException.java
@@ -5,6 +5,8 @@ package uk.ac.cam.groupprojects.bravo.tts;
     Fatal error - means that the system this is being run on hasn't been properly configured.
 */
 public class FestivalMissingException extends Exception {
+    public static final long serialVersionUID = 1L;
+
     public FestivalMissingException(Exception inner) {
         super(inner);
     }
