80c78f2dfe359f81fda357eaeddcaa7bb191607a
adeboyed <david.adeboye@outlook.com>
2018-02-13 12:44:58 +0000

    Add some more to the main function and an applications constants

---
+++ src/main/java/uk/ac/cam/groupprojects/bravo/main/ApplicationConstants.java
@@ -0,0 +1,13 @@
+package uk.ac.cam.groupprojects.bravo.main;
+
+/**
+ * Created by david on 13/02/2018.
+ */
+public class ApplicationConstants {
+
+    public static final String VERSION_NO = "0.1";
+    public static final String PATH_TO_CONFIG = "/home/pi/config.json";
+
+    public static final boolean DEBUG = true;
+
+}
