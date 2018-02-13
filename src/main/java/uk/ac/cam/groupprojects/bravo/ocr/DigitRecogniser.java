93b3dcb1e7765df780436c6ee8cf858d4b448214
Keith <hnefatl@gmail.com>
2018-02-13 21:44:27 +0000

    Added stub recognisor code, test images

---
+++ src/main/java/uk/ac/cam/groupprojects/bravo/ocr/DigitRecogniser.java
@@ -0,0 +1,16 @@
+package uk.ac.cam.groupprojects.bravo.ocr;
+
+public class DigitRecogniser implements AutoCloseable {
+    public DigitRecogniser() {
+
+    }
+
+    public String recognise(String imagePath) {
+        return null;
+    }
+
+    @Override
+    public void close() {
+
+    }
+}
