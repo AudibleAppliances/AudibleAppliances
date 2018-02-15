43d8e8b7d42c2a6d66ab532d74c9d9c930e418c2
adeboyed <david.adeboye@outlook.com>
2018-02-15 13:05:06 +0000

    Add main class for running the applications

--- src/main/java/uk/ac/cam/groupprojects/bravo/main/ApplicationConstants.java
+++ src/main/java/uk/ac/cam/groupprojects/bravo/main/ApplicationConstants.java
@@ -8,6 +8,6 @@ public class ApplicationConstants {
     public static final String VERSION_NO = "0.1";
     public static final String PATH_TO_CONFIG = "/home/pi/config.json";
 
-    public static final boolean DEBUG = true;
+    public static final boolean DEBUG = false;
 
 }
--- src/main/java/uk/ac/cam/groupprojects/bravo/main/AudibleAppliances.java
+++ src/main/java/uk/ac/cam/groupprojects/bravo/main/AudibleAppliances.java
@@ -1,55 +1,127 @@
 package uk.ac.cam.groupprojects.bravo.main;
 
-import static uk.ac.cam.groupprojects.bravo.main.ApplicationConstants.*;
+import uk.ac.cam.groupprojects.bravo.imageProcessing.ConfigException;
+import uk.ac.cam.groupprojects.bravo.imageProcessing.ImageSegments;
+import uk.ac.cam.groupprojects.bravo.tts.FestivalMissingException;
+import uk.ac.cam.groupprojects.bravo.tts.Synthesiser;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import static uk.ac.cam.groupprojects.bravo.main.ApplicationConstants.DEBUG;
+import static uk.ac.cam.groupprojects.bravo.main.ApplicationConstants.
+        VERSION_NO;
+import static uk.ac.cam.groupprojects.bravo.main.ApplicationConstants.
+        PATH_TO_CONFIG;
 
 /**
  * Created by david on 13/02/2018.
  */
 public class AudibleAppliances {
 
-    private static boolean VERBOSE = false;
+    private static Synthesiser synthesiser;
+    private static ImageSegments segments;
+    private static BikeStateTracker bikeStateTracker;
+    private static boolean running = false;
 
     public static void main(String[] args) {
-        if ( args.length > 0 && args[0] == "v" ){
-            VERBOSE = true;
-        }
         printHeader();
 
         //Load config
-        System.out.println("Loading in config...");
-        /*try {
-            ImageSegments segments = new ImageSegments( PATH_TO_CONFIG );
+        System.out.println("Loading in config from " + PATH_TO_CONFIG );
+        try {
 
-        } catch (ConfigException e) {
+            //segments = new ImageSegments( PATH_TO_CONFIG );
+            System.out.println("Config loaded successfully");
+            System.out.println("Setting up required components");
+            bikeStateTracker = new BikeStateTracker( segments );
+            synthesiser = new Synthesiser();
+            System.out.println("Components set up successfully!");
+
+            running = true;
+            new Thread( runTracker ).start();
+
+            Thread inputThread = new Thread( handleInput );
+            inputThread.setDaemon(true);
+            inputThread.start();
+        } catch( FestivalMissingException e ){
             if ( DEBUG )
                 e.printStackTrace();
-            System.out.println("FATAL ERROR: Could not load in file");
+            System.out.println("FATAL ERROR: Could not load voice library!");
             printFooter();
-        } */
+        } catch (Exception e) {
+            if ( DEBUG )
+                e.printStackTrace();
+            System.out.println("FATAL ERROR: Could not load in config");
+            printFooter();
+        }
     }
 
+    static Runnable handleInput = () -> {
+        try {
+            System.out.println("Type 'exit' to close the application");
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader( System.in ) );
+            while ( running ){
+                String input = reader.readLine();
+                if ( input.trim().length() > 0 && input.compareToIgnoreCase("exit") == 0 ){
+                    running = false;
+                    System.out.println("ENDING Audible Appliances");
+                }else {
+                    System.out.println("Type 'exit' to close the application!");
+                }
+            }
+            System.out.println("DONE1");
+        }catch( IOException e ){
+            if ( DEBUG )
+                e.printStackTrace();
+            System.out.println("FATAL ERROR: Cannot read from System.in");
+            printFooter();
+        }
+    };
+
+    static Runnable runTracker = () ->{
+        System.out.println();
+        System.out.println("Starting the bike state tracker! ");
+        synthesiser.speak("Welcome to Audible Appliances");
+
+        while( running ){
+            //Do some shit
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        synthesiser.speak("Goodbye! Hope to see you again soon!");
+        synthesiser.close();
+        printFooter();
+    };
+
     private static void printHeader(){
-        System.out.println("----------------------------------------");
-        System.out.println("----------------------------------------");
-        System.out.println("----------------------------------------");
-        System.out.println("---------- AUDIBLE APPLIANCES ----------");
-        System.out.println("-------------- VERSION " + VERSION_NO + " -------------");
-        System.out.println("------------- DEVELOPED BY: ------------");
-        System.out.println("------------- Oliver Hope --------------");
-        System.out.println("------------ Cameron Ramsay ------------");
-        System.out.println("----------- Keith Collister ------------");
-        System.out.println("------------ David Adeboye -------------");
-        System.out.println("------------ Patryk Balicki ------------");
-        System.out.println("------------ Tom Strudwick -------------");
-        System.out.println("----------------------------------------");
-        System.out.println("----------------------------------------");
+        System.out.println("|----------------------------------------|");
+        System.out.println("|----------------------------------------|");
+        System.out.println("|----------------------------------------|");
+        System.out.println("|---------- AUDIBLE APPLIANCES ----------|");
+        System.out.println("|-------------- VERSION " + VERSION_NO + " -------------|");
+        System.out.println("|------------- DEVELOPED BY: ------------|");
+        System.out.println("|------------- Oliver Hope --------------|");
+        System.out.println("|------------ Cameron Ramsay ------------|");
+        System.out.println("|----------- Keith Collister ------------|");
+        System.out.println("|------------ David Adeboye -------------|");
+        System.out.println("|------------ Patryk Balicki ------------|");
+        System.out.println("|------------ Tom Strudwick -------------|");
+        System.out.println("|----------------------------------------|");
+        System.out.println("|----------------------------------------|");
         System.out.println();
     }
 
     private static void printFooter(){
-        System.out.println("----------------------------------------");
-        System.out.println("---------- AUDIBLE APPLIANCES ----------");
-        System.out.println("----------------------------------------");
+        System.out.println();
+        System.out.println("|----------------------------------------|");
+        System.out.println("|---------- AUDIBLE APPLIANCES ----------|");
+        System.out.println("|----------------------------------------|");
     }
 
 }
