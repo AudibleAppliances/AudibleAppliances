47d9ecc4669c75c76fd58979847aca68737787f9
Cameron R <cameronfr@live.co.uk>
2018-03-03 21:40:13 +0000

    Added taking a photo to the c++

--- main.cpp
+++ main.cpp
@@ -3,22 +3,33 @@
 #include <opencv2/opencv.hpp>
 #include <stdio.h>
 #include <ctime>
+#include <raspicam/raspicam_cv.h>
 using namespace cv;
 using namespace std;
-
 
 
 int main(int argc, char** argv) {
     int type = 16;
     int resolution_x = 1280;
     int resolution_y = 720;
-    double zoom = 1.4;
+    double zoom = 1.2;
     double strength = 2.5;
     if (strength == 0) {
         strength = 0.00001;
     }
     float half_width = resolution_x/2;
     float half_height = resolution_y/2;
+    
+    raspicam::RaspiCam_Cv Camera;
+    Camera.set(CV_CAP_PROP_FORMAT, CV_8UC1);
+    Camera.set(CV_CAP_PROP_FRAME_WIDTH, resolution_x);
+    Camera.set(CV_CAP_PROP_FRAME_HEIGHT, resolution_y);
+    Camera.set(CV_CAP_PROP_EXPOSURE, 20);
+   
+    if (!Camera.open()) {
+	cerr << "Failed to open the camera" << endl;
+    }
+
 
     double correction_radius = sqrt(pow(resolution_x, 2) + pow(resolution_y, 2)) / strength;
 
@@ -54,16 +65,18 @@ int main(int argc, char** argv) {
     }
 
     double total_time = 0;
-
     clock_t begin = clock();
 
-    Mat image = imread("fisheye.jpg");
+    Mat image;
+    Camera.grab();
+    Camera.retrieve(image);
+
     remap(image, *new_image, *mapping_x, *mapping_y, INTER_LINEAR);
     image.release();
     imwrite("new_image.jpg", *new_image);
     clock_t end = clock();
     total_time += double(end - begin) / CLOCKS_PER_SEC;
-
+    Camera.release();
     cout << total_time / 100 << endl;
 
     return 0;
