15416dad50cad842b866478fc1420e9e52e87f8e
Keith <hnefatl@gmail.com>
2018-03-03 20:24:30 +0000

    Added manual thresholding that's faster than the original method - it avoid a monochrome conversion

--- src/main/java/uk/ac/cam/groupprojects/bravo/ocr/SegmentActive.java
+++ src/main/java/uk/ac/cam/groupprojects/bravo/ocr/SegmentActive.java
@@ -2,33 +2,39 @@ package uk.ac.cam.groupprojects.bravo.ocr;
 
 import java.awt.image.BufferedImage;
 import java.awt.image.Raster;
-import java.io.File;
 import java.io.IOException;
 
 public class SegmentActive {
-    // "Threshold" is effectively the percentage (100% = 1.0) of the image that's blue
+    // "Threshold" is effectively the percentage (100% = 1.0) of the image that's white
     public static boolean segmentActive(BufferedImage img) throws IOException {
-        return segmentActive(img, 0.05); // 5% blue is a good default threshold (empirical)
+        return segmentActive(img, 0.05); // 5% white is a good default threshold (empirical)
     }
     public static boolean segmentActive(BufferedImage img, double threshold) throws IOException {
         return imageAverage(img) > threshold;
     }
 
     public static double imageAverage(BufferedImage img) throws IOException {
-        BufferedImage grey = SSOCRUtil.makeMonochrome(img);
-        try { SSOCRUtil.saveFile(grey, new File("out.jpg")); } catch (Exception e) {}
-        Raster raw = grey.getRaster();
+        Raster raw = img.getRaster();
+
+        final int bands = 3;
 
         double sum = 0;
         for (int y = 0; y < raw.getHeight(); y++) {
             for (int x = 0; x < raw.getWidth(); x++) {
-                sum += raw.getSampleDouble(x, y, 0);
+                double pixelAvg = 0;
+                for (int b = 0; b < bands; b++) {
+                    pixelAvg += raw.getSampleDouble(x, y, b);
+                }
+                pixelAvg /= 3 * 255;
+
+                if (pixelAvg > 0.4) {
+                    sum += 1;
+                }
             }
         }
-        sum /= 255.0; // Pixel values are 0-255 (not 0-1, even though they're as doubles)
 
         double n = raw.getWidth() * raw.getHeight();
         double average = sum / n;
-        return 1.0 - average; // Make black = 0.0: it's more intuitive
+        return average;
     }
 }
