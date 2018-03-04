014e7eb3ec31b5abcfca62810ad3500eaaac42c9
David Adeboye <david.adeboye@outlook.com>
2018-03-04 00:35:04 +0000

    I think I fixed it?

--- src/main/java/uk/ac/cam/groupprojects/bravo/imageProcessing/IntelligentCropping.java
+++ src/main/java/uk/ac/cam/groupprojects/bravo/imageProcessing/IntelligentCropping.java
@@ -12,16 +12,19 @@ public class IntelligentCropping {
 
     private static final int THRESHOLD_LIMIT = 40;
     private static final int SEARCH_LIMIT = 2;
+    private static final int HEIGHT_LIMIT = 10; //percentage of height;
 
     public static BufferedImage intelligentCrop( BufferedImage image ){
         FastRGB rgb = new FastRGB( image );
 
         int imageHeight = image.getHeight();
+        int height_limit = imageHeight * 10 / 100;
 
         for ( int i = 0; i < image.getWidth(); i++ ){
             int start = -1;
             int height = 0;
             int limit = SEARCH_LIMIT;
+
             for ( int j = 0; j < limit; j++ ){
                 if ( rgb.getRGB(i, j) > THRESHOLD_LIMIT ){
                     if ( start == -1 )
@@ -30,8 +33,11 @@ public class IntelligentCropping {
                     limit++;
                     height++;
                 }
+                if ( height > height_limit ){
+                    break;
+                }
             }
-            if ( height > 0 && height < ( imageHeight * 0.1 ) ){
+            if ( height > 0 && height < height_limit ){
                 image.setRGB( i, start, 1, height, new int[ height ], 0, 1 );
             }
         }
