c6f19805f9868bbb750c6aeaeb7a7255296c9c62
David Adeboye <david.adeboye@outlook.com>
2018-03-04 00:17:00 +0000

    Change back to sd card

--- src/main/java/uk/ac/cam/groupprojects/bravo/main/SplitImage.java
+++ src/main/java/uk/ac/cam/groupprojects/bravo/main/SplitImage.java
@@ -2,7 +2,9 @@ package uk.ac.cam.groupprojects.bravo.main;
 
 import uk.ac.cam.groupprojects.bravo.config.ConfigData;
 import uk.ac.cam.groupprojects.bravo.imageProcessing.ImageSegments;
+import uk.ac.cam.groupprojects.bravo.imageProcessing.IntelligentCropping;
 import uk.ac.cam.groupprojects.bravo.imageProcessing.ScreenBox;
+import uk.ac.cam.groupprojects.bravo.util.FastImageIO;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
@@ -15,19 +17,42 @@ import java.net.URLDecoder;
 public class SplitImage {
 
     public static void main( String args[] ) throws Exception{
-        String configPath = "src/test/resources/testConfig.json";
-        String imagePath = "src/test/resources/test.jpg";
-        File file = new File("src/test/resources");
-        System.out.println( file.length() );
+        String imagePath = "src/test/resources/input.jpg";
+
+        long startTime = System.currentTimeMillis();
         BufferedImage img = ImageIO.read(new File( imagePath ));
+        long elapsedTime = System.currentTimeMillis() - startTime;
+        System.out.println( "Normal Time " + elapsedTime );
+
+        startTime = System.currentTimeMillis();
+        BufferedImage img1 = FastImageIO.read(new File( imagePath ));
+        elapsedTime = System.currentTimeMillis() - startTime;
+        System.out.println( "Elasped Time " + elapsedTime );
+
+        BufferedImage output = IntelligentCropping.intelligentCrop( img );
+
+        startTime = System.currentTimeMillis();
+        ImageIO.write( output, "jpg", new File( "output/output.jpg" ) );
+        elapsedTime = System.currentTimeMillis() - startTime;
+        System.out.println( "Normal Time " + elapsedTime );
+
+        startTime = System.currentTimeMillis();
+        FastImageIO.write( output, "jpg", new File( "output/output1.jpg" ) );
+        elapsedTime = System.currentTimeMillis() - startTime;
+        System.out.println( "Elasped Time " + elapsedTime );
+
+        /*
+
         ConfigData cd = new ConfigData(configPath);
         ImageSegments segments = new ImageSegments(cd);
 
         //for ( ScreenBox box: ScreenBox.values() ){
             ScreenBox box = ScreenBox.LCD6;
             BufferedImage output = segments.getImageBox( box, img );
-            ImageIO.write( output, "jpg", new File( "output/" + box.toString().toLowerCase() + ".jpg" ) );
+
         //}
+
+        */
 
     }
 
