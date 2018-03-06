e6f1bd44c876799a63344782763e2c1f6cfb26ba
David Adeboye <david.adeboye@outlook.com>
2018-03-06 14:42:38 +0000

    Update run script

--- pi_run.sh
+++ pi_run.sh
@@ -2,18 +2,13 @@
 amixer set PCM -- 100%
 
 # Run GPIO script
-python gpio/gpio.py &
-
-# Move to directory
-cd AudibleAppliances-master
+python AudibleAppliances/gpio/gpio.py &
 
 # Start the python image server script
-./AudibleAppliances-master/daemon/daemon &
+./runserver
 
 # Start the interface web server
-cd webInterface
-python main.py &
+python AudibleAppliances/webInterface/main.py &
 
 # Run the actual program
-cd ..
-java -jar audible-1.0-SNAPSHOT-jar-with-dependencies.jar &
+~/runjar
