b3c3dc5b38a228ab2fe2c0bed96cf620d269b02c
Keith <hnefatl@gmail.com>
2018-03-06 18:34:44 +0000

    Updated intelligent cropping to remove "islands" of pixels that weren't handled before

--- src/main/java/uk/ac/cam/groupprojects/bravo/imageProcessing/IntelligentCropping.java
+++ src/main/java/uk/ac/cam/groupprojects/bravo/imageProcessing/IntelligentCropping.java
@@ -9,12 +9,9 @@ import java.util.List;
 
 import uk.ac.cam.groupprojects.bravo.ocr.SSOCRUtil;
 
-// Improvements to be done:
-// Convex hull on the identified points? Would completely eliminate the islands.
-// Cheap approximation by removing points if all their neighbours are to be removed, even if the point itself wouldn't.
-
 public class IntelligentCropping {
-    private static final double THRESHOLD = 160;
+    private static final double THRESHOLD = 150;
+    private static final double BLACK_THRESHOLD = 20;
 
     private static final double SAFETY_HALT = 0.5; // If we're going to overwrite more of the image than this percent, don't
 
@@ -28,43 +25,80 @@ public class IntelligentCropping {
 
         WritableRaster raw = image.getRaster();
 
+        int earlyStop = (int)(raw.getHeight() * raw.getWidth() * SAFETY_HALT);
         Set<Point> visited = new HashSet<>();
         Set<Point> toOverwrite = new HashSet<>();
-        Set<Point> toVisit = new HashSet<>();
 
-        // Entire front row is initial frontier
-        for (int i = 0; i < raw.getWidth(); i++) {
-            toVisit.add(new Point(i, 0));
+        // Flood from the entire top row, with a reasonable threshold to detect light pixels
+        for (int x = 0; x < raw.getWidth(); x++) {
+            Set<Point> flooded = new HashSet<>();
+            floodFill(raw, new Point(x, 0), THRESHOLD, visited, earlyStop, flooded);
+            if (flooded.size() >= earlyStop) {
+                return; // If we're going to overwrite too much, don't do it
+            }
+            toOverwrite.addAll(flooded);
         }
 
-        Point current;
+        // Actually overwrite the chosen pixels with black
+        blacken(raw, toOverwrite);
 
-        while (!toVisit.isEmpty()) {
-            Iterator<Point> i = toVisit.iterator();
-            current = i.next();
+        // Run another flood fill, this time with a super low threshold to find isolated islands in the
+        // sea of black overwritten pixels. Don't set an early stop, we want to flood as large as possible
+        visited = new HashSet<>();
+        for (int x = 0; x < raw.getWidth(); x++) {
+            for (int y = 0; y < raw.getHeight(); y++) {
+                Point current = new Point(x, y);
+                if (visited.contains(current))
+                    continue;
+
+                Set<Point> flooded = new HashSet<>();
+                floodFill(raw, new Point(x, y), BLACK_THRESHOLD, visited, Integer.MAX_VALUE, flooded);
+                if (flooded.size() >= earlyStop) {
+                    continue; // If we've flooded a large area, we've got a significantly large island so don't remove it
+                }
+
+                blacken(raw, flooded);
+            }
+        }
+    }
+
+    private static void blacken(WritableRaster raw, Set<Point> points) {
+        for (Point p : points) {
+            raw.setPixel(p.x, p.y, new double[] { 0, 0, 0 });
+        }
+    }
+    
+    private static void floodFill(Raster raw, Point start, double threshold, Set<Point> visited,
+                                     int earlyStop, Set<Point> outputFlood) {
+        Set<Point> frontier = new HashSet<>();
+        frontier.add(start);
+
+        Set<Point> flooded = new HashSet<>();
+        while (!frontier.isEmpty()) {
+            Iterator<Point> i = frontier.iterator();
+            Point current = i.next();
             i.remove();
+
             visited.add(current);
-            if (!threshTest(raw, current)) {
+            if (!isAboveThreshold(raw, current, threshold)) {
                 // If we don't need to remove this pixel, move on.
                 // Don't consider its neighbours and don't overwrite it
                 continue;
             }
-            toOverwrite.add(current);
+
+            flooded.add(current);
+            if (flooded.size() > earlyStop) { // If we're going to overwrite too many pixels, stop
+                break;
+            }
 
             for (Point neighbour : getNeighbours(current, raw)) {
                 if (!visited.contains(neighbour)) {
-                    toVisit.add(neighbour);
+                    frontier.add(neighbour);
                 }
             }
         }
 
-        // If we're going to overwrite more than 50% of the image........ don't.
-        int area = raw.getWidth() * raw.getHeight();
-        if (toOverwrite.size() < area * SAFETY_HALT) {
-            for (Point p : toOverwrite) {
-                raw.setPixel(p.x, p.y, new double[] { 0, 0, 0 });
-            }
-        }
+        outputFlood.addAll(flooded);
     }
 
     /**
@@ -74,7 +108,7 @@ public class IntelligentCropping {
      * @param raw
      * @return
      */
-    private static List<Point> getNeighbours(Point p, WritableRaster raw) {
+    private static List<Point> getNeighbours(Point p, Raster raw) {
         List<Point> l = new ArrayList<>();
         int maxX = raw.getWidth();
         int maxY = raw.getHeight();
@@ -98,9 +132,9 @@ public class IntelligentCropping {
      * @param p
      * @return
      */
-    private static boolean threshTest(Raster raw, Point p) {
+    private static boolean isAboveThreshold(Raster raw, Point p, double threshold) {
         // Based on empirical testing
-        return raw.getSampleDouble(p.x, p.y, 1) > THRESHOLD;
+        return raw.getSampleDouble(p.x, p.y, 1) > threshold;
     }
 
 }
