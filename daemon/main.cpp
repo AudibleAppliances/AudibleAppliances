83a1023736ab1cb0e80dc8f0fca770bf41913867
Cameron R <cameronfr@live.co.uk>
2018-03-06 19:23:35 +0000

    Removed debug statements

--- daemon/main.cpp
+++ daemon/main.cpp
@@ -60,7 +60,7 @@ int main(int argc, char *argv[]) {
 
     while (true) {
         int client_socket = accept(server_socket, (struct sockaddr *)&address, (socklen_t*)&address_length);
-        std::cout << "New reader" << std::endl;
+        std::cout << "New reader connected" << std::endl;
         std::thread reader_thread (reader, client_socket, std::ref(active_readers),
                                    std::ref(turn), std::ref(waiting_readers), std::ref(write_guard));
         reader_thread.detach();
@@ -99,43 +99,18 @@ void reader(int socket, std::atomic_int &active_readers, Semaphore &turn,
     while (true) {
         read(socket, buffer, 1);
         if (buffer[0] == 2) {
-            std::cout << "========================================================\n"
-                      << "=======================New Reader=======================\n"
-                      << "========================================================" << std::endl;
-            
-        clock_t begin = clock();
-	    std::cout << "Reader thread: Acquiring turn" << std::endl;
-	    turn.wait();
-        clock_t clock1 = clock();
-        std::cout << "Reader thread: turn.wait() in " << double(clock1 - begin) / CLOCKS_PER_SEC << "s" << std::endl;
-	    turn.signal();
-        clock_t clock2 = clock();
-        std::cout << "Reader thread: turn.signal() in " << double(clock2 - clock1) / CLOCKS_PER_SEC << "s" << std::endl;
+            turn.wait();
+            turn.signal();
 
-	    std::cout << "Reader thread: Acquiring waiting_readers" << std::endl;
-	    waiting_readers.wait();
-        clock_t clock3 = clock();
-        std::cout << "Reader thread: waiting_readers.wait() in " << double(clock3 - clock2) / CLOCKS_PER_SEC << "s" << std::endl;
-	    if (std::atomic_load(&active_readers) == 0) {
-	        write_guard.wait();
-	    }
-        clock_t clock4 = clock();
-        std::cout << "Reader thread: test and write_guard.wait() in " << double(clock4 - clock3) / CLOCKS_PER_SEC << "s" << std::endl;
-	    std::atomic_fetch_add(&active_readers, 1);
-        clock_t clock5 = clock();
-        std::cout << "Reader thread: std::atomic_fetch_add(&active_readers, 1); in " << double(clock5 - clock4) / CLOCKS_PER_SEC << "s" << std::endl;
-	    waiting_readers.signal();
-        clock_t clock6 = clock();
-        std::cout << "Reader thread: waiting_readers.signal() in " << double(clock6 - clock5) / CLOCKS_PER_SEC << "s" << std::endl;
-	    std::cout << "Reader thread: Sending 1 to client socket" << std::endl;
-	    send(socket, response, 1, 0);
-        clock_t clock7 = clock();
-        std::cout << "Reader thread: send(socket, response, 1, 0) in " << double(clock6 - clock5) / CLOCKS_PER_SEC << "s" << std::endl;
-
-//            std::cout << "Reader thread: Wait to hear from client to read" << std::endl;
+            waiting_readers.wait();
+            if (std::atomic_load(&active_readers) == 0) {
+                write_guard.wait();
+            }
+            std::atomic_fetch_add(&active_readers, 1);
+            waiting_readers.signal();
+            send(socket, response, 1, 0);
             read(socket, buffer, 1);
 
-//            std::cout << "Reader thread: Acquiring waiting_readers" << std::endl;
             waiting_readers.wait();
             std::atomic_fetch_sub(&active_readers, 1);
             if (std::atomic_load(&active_readers) == 0) {
