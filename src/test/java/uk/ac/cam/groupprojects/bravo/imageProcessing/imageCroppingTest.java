771c3940d635e4b5665d5cb5ba563134a8cb0bcc
Oliver Hope <oliverdnhope@gmail.com>
2018-03-06 15:12:41 +0000

    Get more lines

--- src/test/java/uk/ac/cam/groupprojects/bravo/imageProcessing/imageCroppingTest.java
+++ src/test/java/uk/ac/cam/groupprojects/bravo/imageProcessing/imageCroppingTest.java
@@ -15,6 +15,7 @@ public class imageCroppingTest {
         //String imgPath = URLDecoder.decode(getClass().getResource("/lcd1.jpg").getFile(), "UTF-8");
         //BufferedImage img = ImageIO.read(Paths.get(imgPath).toFile());
         //IntelligentCropping.intelligentCrop(img);
+
         //String writePath = "/Users/User/Desktop/lcd2-intellicrop.jpg";
         //ImageIO.write(img, "jpg", Paths.get(writePath).toFile());
     }
