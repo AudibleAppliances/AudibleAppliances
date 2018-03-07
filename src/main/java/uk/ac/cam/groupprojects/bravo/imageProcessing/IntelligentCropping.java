c4980d6626ea3faf41a71397d194084950aa2076
Keith <hnefatl@gmail.com>
2018-03-07 01:23:59 +0000

    Un-reverted the optimisations to crops

--- src/main/java/uk/ac/cam/groupprojects/bravo/imageProcessing/IntelligentCropping.java
+++ src/main/java/uk/ac/cam/groupprojects/bravo/imageProcessing/IntelligentCropping.java
@@ -1,12 +1,14 @@
 package uk.ac.cam.groupprojects.bravo.imageProcessing;
 
-import java.awt.*;
+import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 import java.awt.image.Raster;
 import java.awt.image.WritableRaster;
 import java.util.*;
 
 import uk.ac.cam.groupprojects.bravo.ocr.SSOCRUtil;
+
+// Beware - here be the dragons of optimisation
 
 public class IntelligentCropping {
     private static final double THRESHOLD = 150;
@@ -26,14 +28,17 @@ public class IntelligentCropping {
 
         WritableRaster raw = image.getRaster();
 
+        // We treat the visited array as a 1D array of integers which map to the 2D pixel grid in the normal way:
+        // i = width * y + x
+        // Efficiency...
         int earlyStop = (int)(raw.getHeight() * raw.getWidth() * SAFETY_HALT);
-        boolean[][] visited = new boolean[raw.getHeight()][raw.getWidth()];
-        Set<Point> toOverwrite = new HashSet<>();
+        boolean[] visited = new boolean[raw.getHeight() * raw.getWidth()];
+        Set<Integer> toOverwrite = new HashSet<>();
 
         // Flood from the entire top row, with a reasonable threshold to detect light pixels
         for (int x = 0; x < raw.getWidth(); x++) {
-            Set<Point> flooded = new HashSet<>();
-            floodFill(raw, new Point(x, 0), THRESHOLD, visited, earlyStop, flooded);
+            Set<Integer> flooded = new HashSet<>();
+            floodFill(raw, x, THRESHOLD, visited, earlyStop, flooded);
             if (flooded.size() >= earlyStop) {
                 return; // If we're going to overwrite too much, don't do it
             }
@@ -50,28 +55,29 @@ public class IntelligentCropping {
         // Create a sub-raster that forms a bounding rectangle of the area that we overwrote pixels in
         // We operate on this smaller raster in the second pass, as we can ignore anything that we didn't touch
         // in the first pass
-        Rectangle bounds = getBounds(toOverwrite);
+        Rectangle bounds = getBounds(raw, toOverwrite);
         WritableRaster sub = raw.createWritableChild(bounds.x, bounds.y, bounds.width, bounds.height, 0, 0, null);
         earlyStop = (int)(sub.getWidth() * sub.getHeight() * SAFETY_HALT);
 
         // Run another flood fill, this time with a super low threshold to find isolated islands in the
         // sea of black overwritten pixels. Don't set an early stop, we want to flood as large as possible
-        visited = new boolean[sub.getHeight()][sub.getWidth()];
+        visited = new boolean[sub.getHeight() * sub.getWidth()];
 
         // Add all the points that we previously filled with black to the visited set, as we don't have to check them
-        for (Point p : toOverwrite) {
-            p.translate(-bounds.x, -bounds.y);
-            visited[p.y][p.x] = true;
+        for (int p : toOverwrite) {
+            int px = p % raw.getWidth() - bounds.x;
+            int py = p / raw.getWidth() - bounds.y;
+            visited[py * bounds.width + px] = true; // Translate by (-bounds.x, -bounds.y)
         }
-        for (int x = 0; x < sub.getWidth(); x++) {
-            for (int y = 0; y < sub.getHeight(); y++) {
-                Point current = new Point(x, y);
-                if (visited[current.y][current.x]) {
+        for (int y = 0; y < sub.getHeight(); y++) {
+            for (int x = 0; x < sub.getWidth(); x++) {
+                int current = y * sub.getWidth() + x;
+                if (visited[current]) {
                     continue;
                 }
 
-                Set<Point> flooded = new HashSet<>();
-                floodFill(sub, new Point(x, y), BLACK_THRESHOLD, visited, Integer.MAX_VALUE, flooded);
+                Set<Integer> flooded = new HashSet<>();
+                floodFill(sub, current, BLACK_THRESHOLD, visited, Integer.MAX_VALUE, flooded);
 
                 if (flooded.size() >= earlyStop) {
                     continue; // If we've flooded a large area, we've got a significantly large island so don't remove it
@@ -82,24 +88,24 @@ public class IntelligentCropping {
         }
     }
 
-    private static void blacken(WritableRaster raw, Set<Point> points) {
-        for (Point p : points) {
-            raw.setPixel(p.x, p.y, FILL_COLOUR);
+    private static void blacken(WritableRaster raw, Set<Integer> points) {
+        for (int p : points) {
+            raw.setPixel(p % raw.getWidth(), p / raw.getWidth(), FILL_COLOUR);
         }
     }
     
-    private static void floodFill(Raster raw, Point start, double threshold, boolean[][] visited,
-                                  int earlyStop, Set<Point> outputFlood) {
-        Set<Point> frontier = new HashSet<>();
+    private static void floodFill(Raster raw, int start, double threshold, boolean[] visited,
+                                  int earlyStop, Set<Integer> outputFlood) {
+        Set<Integer> frontier = new HashSet<>();
         frontier.add(start);
 
-        Set<Point> flooded = new HashSet<>();
+        Set<Integer> flooded = new HashSet<>();
         while (!frontier.isEmpty()) {
-            Iterator<Point> i = frontier.iterator();
-            Point p = i.next();
+            Iterator<Integer> i = frontier.iterator();
+            int p = i.next();
             i.remove();
 
-            visited[p.y][p.x] = true;
+            visited[p] = true;
             if (!isAboveThreshold(raw, p, threshold)) {
                 // If we don't need to remove this pixel, move on.
                 // Don't consider its neighbours and don't overwrite it
@@ -112,28 +118,33 @@ public class IntelligentCropping {
             }
 
             // Compute neighbours. Done nastily here because it's faster than eg. using a helper function
-            if (p.x + 1 < raw.getWidth() && !visited[p.y][p.x + 1])
-                frontier.add(new Point(p.x + 1, p.y));
-            if (p.x - 1 >= 0 && !visited[p.y][p.x - 1])
-                frontier.add(new Point(p.x - 1, p.y));
-            if (p.y + 1 < raw.getHeight() && !visited[p.y + 1][p.x])
-                frontier.add(new Point(p.x, p.y + 1));
-            if (p.y - 1 >= 0 && !visited[p.y - 1][p.x])
-                frontier.add(new Point(p.x, p.y - 1));
+            int px = p % raw.getWidth();
+            int py = p / raw.getWidth();
+
+            if (px + 1 < raw.getWidth() && !visited[p + 1])
+                frontier.add(p + 1);
+            if (px - 1 >= 0 && !visited[p - 1])
+                frontier.add(p - 1);
+            if (py + 1 < raw.getHeight() && !visited[p + raw.getWidth()])
+                frontier.add(p + raw.getWidth());
+            if (py - 1 >= 0 && !visited[p - raw.getWidth()])
+                frontier.add(p - raw.getWidth());
         }
 
         outputFlood.addAll(flooded);
     }
 
-    private static Rectangle getBounds(Set<Point> points) {
+    private static Rectangle getBounds(Raster raw, Set<Integer> points) {
         int x0 = Integer.MAX_VALUE, y0 = Integer.MAX_VALUE;
-        int x1 = 1, y1 = 1; // The minimum allowed width/height are 1
+        int x1 = 0, y1 = 0; // The minimum allowed width/height are 1
 
-        for (Point p : points) {
-            x0 = Math.min(p.x, x0);
-            y0 = Math.min(p.y, y0);
-            x1 = Math.max(p.x, x1);
-            y1 = Math.max(p.y, y1);
+        for (int p : points) {
+            int px = p % raw.getWidth();
+            int py = p / raw.getWidth();
+            x0 = Math.min(px, x0);
+            y0 = Math.min(py, y0);
+            x1 = Math.max(px, x1);
+            y1 = Math.max(py, y1);
         }
 
         int width = x1 - x0 + 1;
@@ -149,8 +160,7 @@ public class IntelligentCropping {
      * @param p
      * @return
      */
-    private static boolean isAboveThreshold(Raster raw, Point p, double threshold) {
-        // Based on empirical testing
-        return raw.getSampleDouble(p.x, p.y, 1) > threshold;
+    private static boolean isAboveThreshold(Raster raw, Integer p, double threshold) {
+        return raw.getSampleDouble(p % raw.getWidth(), p / raw.getWidth(), 1) > threshold;
     }
 }
