33d038b07d942d4d52506b8ebba9431f5413542e
David Adeboye <david.adeboye@outlook.com>
2018-03-07 12:05:35 +0000

    Even faster image io

--- src/main/java/uk/ac/cam/groupprojects/bravo/util/FastImageIO.java
+++ src/main/java/uk/ac/cam/groupprojects/bravo/util/FastImageIO.java
@@ -2,33 +2,45 @@ package uk.ac.cam.groupprojects.bravo.util;
 
 import com.sun.image.codec.jpeg.JPEGCodec;
 import com.sun.image.codec.jpeg.JPEGImageDecoder;
+import com.sun.image.codec.jpeg.JPEGImageEncoder;
+import com.sun.imageio.plugins.jpeg.JPEGImageWriter;
 
 import javax.imageio.ImageIO;
+import javax.imageio.ImageTypeSpecifier;
+import javax.imageio.ImageWriter;
+import javax.imageio.metadata.IIOMetadata;
+import javax.imageio.stream.ImageOutputStream;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferByte;
 import java.awt.image.Raster;
 import java.awt.image.RenderedImage;
 import java.io.*;
+import java.nio.ByteBuffer;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
+import java.util.Iterator;
 
 /**
  * Created by david on 03/03/2018.
  */
 public class FastImageIO {
 
-    public static void write(RenderedImage im, String format, File outputFile ) throws IOException {
-        BufferedOutputStream imageOutputStream = new BufferedOutputStream(new FileOutputStream(outputFile));
-        ImageIO.write(im, format, imageOutputStream);
-        imageOutputStream.close();
-
+    public static void write(BufferedImage im, String format, File outputFile ) throws IOException {
+        FileChannel fileChannel = new RandomAccessFile(outputFile, "rw").getChannel();
+        DataBufferByte bufferByte = (DataBufferByte) im.getRaster().getDataBuffer();
+        ByteBuffer buffer = MappedByteBuffer.wrap( bufferByte.getData() );
+        buffer.flip();
+        fileChannel.write( buffer );
+        fileChannel.close();
     }
 
-    public static BufferedImage read(File inputFile ) throws IOException {
+
+    public static BufferedImage read(File inputFile) throws IOException {
         FileChannel fileChannel = new RandomAccessFile(inputFile, "r").getChannel();
         MappedByteBuffer buffer = fileChannel.map(FileChannel.MapMode.READ_ONLY, 0, fileChannel.size());
         byte[] imageBuffer = new byte[(int) inputFile.length()];
         buffer.get( imageBuffer );
+        fileChannel.close();
         return decodeJPEG( imageBuffer );
     }
 
@@ -49,6 +61,12 @@ public class FastImageIO {
 
         // Translate the decoded Raster to the specified location and return.
         return image;
+    }
+
+    private static byte[] encodeJPEG( BufferedImage data ){
+        ByteArrayOutputStream jpegStream = new ByteArrayOutputStream();
+
+        return null;
     }
 
 }
