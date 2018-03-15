87690faa967859f8eddae977c941a5baeeec6941
Keith <hnefatl@gmail.com>
2018-03-15 14:40:36 +0000

    Removed clear screen

--- src/main/java/uk/ac/cam/groupprojects/bravo/main/AudibleAppliances.java
+++ src/main/java/uk/ac/cam/groupprojects/bravo/main/AudibleAppliances.java
@@ -98,9 +98,6 @@ public class AudibleAppliances {
 
         int connectionAttempts = 0;
         while (true) {
-            System.out.print("\033[H\033[2J");  
-            System.out.flush();  
-
             long overallStart = System.currentTimeMillis();
             try {
                 long start = System.currentTimeMillis();
