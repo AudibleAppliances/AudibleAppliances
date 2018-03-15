1153d7ed47d5d503a9b5766fcb233734ce17864e
David Adeboye <david.adeboye@outlook.com>
2018-03-15 12:45:27 +0000

    Remove useless sun classes

--- src/main/java/uk/ac/cam/groupprojects/bravo/util/FastImageIO.java
+++ src/main/java/uk/ac/cam/groupprojects/bravo/util/FastImageIO.java
@@ -3,19 +3,25 @@ package uk.ac.cam.groupprojects.bravo.util;
 import com.sun.image.codec.jpeg.JPEGCodec;
 import com.sun.image.codec.jpeg.JPEGImageDecoder;
 
+import javax.imageio.ImageIO;
+import javax.imageio.ImageReader;
+import javax.imageio.stream.MemoryCacheImageInputStream;
 import java.awt.image.BufferedImage;
 import java.awt.image.DataBufferByte;
 import java.io.*;
 import java.nio.ByteBuffer;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
+import java.util.Iterator;
 
 /**
  * Created by david on 03/03/2018.
  */
 public class FastImageIO {
 
-    public static void write(BufferedImage im, String format, File outputFile ) throws IOException {
+    private static final String JPGMime = "image/jpeg";
+
+    public static void write(BufferedImage im, File outputFile ) throws IOException {
         FileChannel fileChannel = new RandomAccessFile(outputFile, "rw").getChannel();
         DataBufferByte bufferByte = (DataBufferByte) im.getRaster().getDataBuffer();
         ByteBuffer buffer = MappedByteBuffer.wrap( bufferByte.getData() );
@@ -34,23 +40,21 @@ public class FastImageIO {
         return decodeJPEG( imageBuffer );
     }
 
-    private static BufferedImage decodeJPEG(byte[] data) {
+    private static BufferedImage decodeJPEG(byte[] data) throws IOException {
         // Create an InputStream from the compressed data array.
         ByteArrayInputStream jpegStream = new ByteArrayInputStream(data);
 
-        // Create a decoder.
-        JPEGImageDecoder decoder = JPEGCodec.createJPEGDecoder(jpegStream);
-
-        // Decode the compressed data into a Raster.
-        BufferedImage image;
-        try {
-            image = decoder.decodeAsBufferedImage();
-        } catch (IOException ioe) {
-            throw new RuntimeException("TIFFImage13");
+        ImageReader JPGReader;
+        Iterator<ImageReader> JPGReaderIter =
+                ImageIO.getImageReadersByMIMEType(JPGMime);
+        if(JPGReaderIter.hasNext()) {
+            JPGReader = JPGReaderIter.next();
+            JPGReader.setInput(new
+                    MemoryCacheImageInputStream(jpegStream));
+            return JPGReader.read(0);
+        }else {
+            throw new IOException("Not a JPEG");
         }
-
-        // Translate the decoded Raster to the specified location and return.
-        return image;
     }
 
     private static byte[] encodeJPEG( BufferedImage data ){
