05f970ff24ca8353612f4bc1fec9a5f0fca87da8
Cameron R <cameronfr@live.co.uk>
2018-03-15 21:48:15 +0000

    Updated LCD names

--- webInterface/main.py
+++ webInterface/main.py
@@ -57,10 +57,10 @@ def new_box():
         config["spoken_fields"][content_type] = spoken
         if content_type == "lcd_text_10" or content_type == "lcd_text_11":
             config["boxes"]["program"] = {
-                left_percentage: config["boxes"]["text_10"]["left_percentage"],
-                top_percentage: min(config["boxes"]["text_10"]["top_percentage"], config["boxes"]["text_11"]["top_percentage"]),
-                width: config["boxes"]["text_10"]["width"] + config["boxes"]["text_11"]["width"],
-                height: max(config["boxes"]["text_10"]["height"], config["boxes"]["text_11"]["height"])
+                left_percentage: config["boxes"]["lcd_text_10"]["left_percentage"],
+                top_percentage: min(config["boxes"]["lcd_text_10"]["top_percentage"], config["boxes"]["lcd_text_11"]["top_percentage"]),
+                width: config["boxes"]["lcd_text_10"]["width"] + config["boxes"]["lcd_text_11"]["width"],
+                height: max(config["boxes"]["lcd_text_10"]["height"], config["boxes"]["lcd_text_11"]["height"])
             }
     with open(config_file, 'w') as fp:
         json.dump(config, fp)
