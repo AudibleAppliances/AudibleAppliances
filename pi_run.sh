71539486083c99aab53ba1d2d38b6bc79c22e51a
Keith <hnefatl@gmail.com>
2018-03-16 03:03:56 +0000

    Rewrote startup kill stuff

--- pi_run.sh
+++ pi_run.sh
@@ -1,17 +1,13 @@
 # Run GPIO script
 python /home/pi/AudibleAppliances/gpio/gpio.py &>/dev/null &
-echo $! >  pids
 
 # Start the python image server script
 /home/pi/runserver &>/dev/null &
-echo $! >> pids
 
 sleep 5
 
 # Start the interface web server
 /home/pi/runwebserver &>/dev/null &
-echo $! >> pids
 
 # Run the actual program
 /home/pi/runjar &>/dev/null &
-echo $! >> pids
