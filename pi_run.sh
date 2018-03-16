71f38dfda0a7b3f0cd276eb075e16245b1f3747c
Cameron R <cameronfr@live.co.uk>
2018-03-16 02:01:19 +0000

    Updated pi_run.sh

--- pi_run.sh
+++ pi_run.sh
@@ -1,23 +1,11 @@
-#Increase volume to 100%
-amixer set PCM -- 100%
-
-wget -q --spider http://google.com
-
-if [ $? -eq 0 ]; then
-    echo "Online"
-else
-    echo "Offline"
-fi
-
-
 # Run GPIO script
-python AudibleAppliances/gpio/gpio.py &
+python /home/pi/AudibleAppliances/gpio/gpio.py &
 
 # Start the python image server script
-./runserver &
+./home/pi/runserver &
 
 # Start the interface web server
-./runwebserver &
+./home/pi/runwebserver &
 
 # Run the actual program
-~/runjar
+./home/pi/runjar
