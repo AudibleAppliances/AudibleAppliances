67ad9ffd3fa50d0f431f979a9c7b80da20c95158
Keith <hnefatl@gmail.com>
2018-03-17 03:47:15 +0000

    Added the actual file this time

---
+++ src/main/java/uk/ac/cam/groupprojects/bravo/model/menu/LoadChangingScreen.java
@@ -0,0 +1,49 @@
+package uk.ac.cam.groupprojects.bravo.model.menu;
+
+import java.util.Arrays;
+import java.util.List;
+
+import uk.ac.cam.groupprojects.bravo.config.BikeField;
+import uk.ac.cam.groupprojects.bravo.imageProcessing.ScreenBox;
+import uk.ac.cam.groupprojects.bravo.main.BikeStateTracker;
+import uk.ac.cam.groupprojects.bravo.model.LCDState;
+import uk.ac.cam.groupprojects.bravo.model.numbers.Load;
+
+// This is effectively a substate of the RunningScreen, specifically when dial's being rotated
+// The "Load" indicator LCD lights up and we see the load level on LCD5
+public class LoadChangingScreen extends BikeScreen {
+    @Override
+    public boolean isActiveScreen(BikeStateTracker state) {
+        boolean active = state.isTimeChanging() &&
+                           state.getBoxState(ScreenBox.LCD_TEXT_1) == LCDState.SOLID_OFF &&
+                           state.getBoxState(ScreenBox.LCD_TEXT_4) == LCDState.SOLID_ON &&
+                           state.getBoxState(ScreenBox.LCD_TEXT_5_TOP) == LCDState.SOLID_ON &&
+                           state.getBoxState(ScreenBox.LOAD) == LCDState.SOLID_ON;
+
+        return active;
+    }
+
+    @Override
+    public ScreenEnum getEnum() {
+        return ScreenEnum.LOAD_CHANGING_SCREEN;
+    }
+
+    @Override
+    public List<String> formatSpeech(BikeStateTracker bikeStateTracker) {
+        Load load = (Load)bikeStateTracker.getFieldValue(BikeField.LOAD, false);
+        if (load == null)
+            return Arrays.asList();
+
+        return Arrays.asList("Difficulty level " + load.getValue());
+    }
+
+    @Override
+    public int getSpeakDelay() {
+        return 3000;
+    }
+
+    @Override
+    public boolean isSpeakFirst() {
+        return true;
+    }
+}
