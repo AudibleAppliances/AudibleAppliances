e2767e11e8ac2c54d8eda070874e359a06bfe1ba
Keith <hnefatl@gmail.com>
2018-03-17 02:54:09 +0000

    More edits to pi_run

--- pi_run.sh
+++ pi_run.sh
@@ -1,15 +1,16 @@
+cd /home/pi
 sleep 5
 
 # Run GPIO script
-python /home/pi/AudibleAppliances/gpio/gpio.py &>/dev/null &
+python AudibleAppliances/gpio/gpio.py &>/dev/null &
 
 # Start the python image server script
-/home/pi/runserver &>/dev/null &
+runserver &>/dev/null &
 
 sleep 5
 
 # Start the interface web server
-/home/pi/runwebserver &>/dev/null &
+runwebserver &>/dev/null &
 
 # Run the actual program
-/home/pi/runjar &>/dev/null &
+runjar &>/dev/null &
