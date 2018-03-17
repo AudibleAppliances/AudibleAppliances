b2506b380f91dad7cd624f8001708df705b24172
Keith <hnefatl@gmail.com>
2018-03-17 02:56:36 +0000

    More updates to pi_run

--- pi_run.sh
+++ pi_run.sh
@@ -1,5 +1,7 @@
 cd /home/pi
+
 sleep 5
+echo "Audible Appliances: Starting GPIO and image server"
 
 # Run GPIO script
 python AudibleAppliances/gpio/gpio.py &>/dev/null &
@@ -8,9 +10,12 @@ python AudibleAppliances/gpio/gpio.py &>/dev/null &
 runserver &>/dev/null &
 
 sleep 5
+echo "Audible Appliances: Starting web server and main program"
 
 # Start the interface web server
 runwebserver &>/dev/null &
 
 # Run the actual program
 runjar &>/dev/null &
+
+pwd
